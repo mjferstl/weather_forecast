f47e60a1072e91a11a15ddc7f2525b77e1568f1c
mjferstl <39524509+mjferstl@users.noreply.github.com>
2018-06-01 15:28:04 +0200

    Add files via upload
    variables declared global instead of local at every use

--- weather.js
+++ weather.js
@@ -15,13 +15,13 @@ var forecastTemp = [];
 google.charts.load('current', {'packages':['corechart'],'language':'de'});
 google.charts.setOnLoadCallback(getWeatherData);
 //
+var weatherURL, weatherData;
 //
 function getWeatherData(){
-	var weatherURL = URLbase + 'weather?id=' + cities[aktCity] + '&units=metric&APPID=' + apikey;
-	$("#Seite").html(initLayout);
+	weatherURL = URLbase + 'weather?id=' + cities[aktCity] + '&units=metric&APPID=' + apikey;
 	$.getJSON(weatherURL, function(data) {
 		//$("#test").html(JSON.stringify(data));
-		var weatherData = JSON.parse(JSON.stringify(data));
+		weatherData = JSON.parse(JSON.stringify(data));
 		$("#cityTitle").html('Wetter in '+ aktCity);
 		updateWeather(weatherData,"#wetter");
 		string = "#wetter";
@@ -33,44 +33,48 @@ function getWeatherData(){
 	});
 }
 //
+var cityName, temp, pressure, humidity, minTemp, maxTemp, windSpeed, iconNr, iconURL, timestamp;
+var date, year, month, day, hours, minutes, seconds, formattedTime;
 // update the weather data
 function updateWeather(weatherData,element){
 
 	$(element).text('');
 
-	var cityName = weatherData.name;
-	var temp = weatherData.main.temp;
-	var pressure = weatherData.main.pressure;
-	var humidity = weatherData.main.humidity;
-	var minTemp = weatherData.main.temp_min;
-	var maxTemp = weatherData.main.temp_max;
-	var windSpeed = weatherData.wind.speed;
-	var iconNr = weatherData.weather[0].icon;	
-	var iconURL = 'https://openweathermap.org/img/w/' + iconNr + '.png';
-	var timestamp = weatherData.dt;
+	cityName = weatherData.name;
+	temp = weatherData.main.temp;
+	pressure = weatherData.main.pressure;
+	humidity = weatherData.main.humidity;
+	minTemp = weatherData.main.temp_min;
+	maxTemp = weatherData.main.temp_max;
+	windSpeed = weatherData.wind.speed;
+	iconNr = weatherData.weather[0].icon;	
+	iconURL = 'https://openweathermap.org/img/w/' + iconNr + '.png';
+	timestamp = weatherData.dt;
 	
-	var date = new Date(timestamp*1000);
-	var year = "" + date.getFullYear();
-	var month = date.getMonth()+1;
-	var day = date.getDate();
-	var hours = date.getHours();
-	var minutes = "0" + date.getMinutes();
-	var seconds = "0" + date.getSeconds();
+	date = new Date(timestamp*1000);
+	year = "" + date.getFullYear();
+	month = date.getMonth()+1;
+	day = date.getDate();
+	hours = date.getHours();
+	minutes = "0" + date.getMinutes();
+	seconds = "0" + date.getSeconds();
 	// Will display time in 10:30:23 format
-	var formattedTime = day + '.' + month + '. - ' + hours + ':' + minutes.substr(-2) + '&nbsp';//:' + seconds.substr(-2);
+	formattedTime = day + '.' + month + '. - ' + hours + ':' + minutes.substr(-2) + '&nbsp';//:' + seconds.substr(-2);
 	
 	$(element).append('<div width="100%" class="Zeitpunkt" style="border-bottom: 0.5px solid">' + formattedTime + '</div>');
 	$(element).append('<table width="100%"><tr><td style="text-align:center;border-bottom-left-radius:20px"><img src="'+iconURL+'" style="height=100%;vertical-align:middle"></td><td width="50%" style="background-color:#33adff;text-align:left;border-bottom-right-radius:20px;padding:0px 10px 0px 10px"><b>' + parseInt(temp) + '&nbsp&deg;C</b><br>' + parseInt(pressure) + '&nbsphPa<br>' + parseInt(humidity) + '&nbsp%<br>' + parseInt(windSpeed) + '&nbspm/s</td></tr></table>');
 }
-
+//
+var aktTemp, dataTable, forecastURL, forecastData, options, chart;
+//
 function updateForecast(wetterAktuell) {
 	//
 	// Wetter aktuell fuer den Plot
-	var timestamp = wetterAktuell.dt;
-	var aktTemp = wetterAktuell.main.temp;
+	timestamp = wetterAktuell.dt;
+	aktTemp = wetterAktuell.main.temp;
 	//
 	// DataTable fuer den Plot erstellen
-	var dataTable = new google.visualization.DataTable();
+	dataTable = new google.visualization.DataTable();
 	// Spalte 1: Datum, Zeit
 	dataTable.addColumn('date', 'Zeit');
 	// Spalte 2: Temperatur
@@ -81,11 +85,11 @@ function updateForecast(wetterAktuell) {
 	// Wettervorhersage laden und auslesen
 	//
 	// URL zum Abruf der Daten
-	var forecastURL = URLbase + 'forecast?id=' + cities[aktCity] + '&units=metric&APPID=' + apikey;
+	forecastURL = URLbase + 'forecast?id=' + cities[aktCity] + '&units=metric&APPID=' + apikey;
 	// JSON-Antwort auslesen
 	$.getJSON(forecastURL, function(data) {
 		// JSON
-		var forecastData = JSON.parse(JSON.stringify(data));
+		forecastData = JSON.parse(JSON.stringify(data));
 		// Fuer jeden Vorhersagezeitpunkt ein Element erstellen und die Werte zur Plot-Tabelle hinzufuegen
 		for (iter = 0; iter < forecastData.list.length; iter++) {
 			if (count == maxCols) {
@@ -102,11 +106,11 @@ function updateForecast(wetterAktuell) {
 			$(string).css({"background-color":"#e9e7e2","border-radius":"20px","padding":"0px","margin":"15px 0px 15px"});
 			updateWeather(forecastData.list[iter],string);
 			
-			var timestamp = forecastData.list[iter].dt;			
+			timestamp = forecastData.list[iter].dt;			
 			dataTable.addRow([new Date(timestamp*1000), forecastData.list[iter].main.temp]);
 		}
 		// Plot definieren
-		var options = {
+		options = {
           title: 'Temperaturen in den nächsten Tagen in °C',
 		  titleTextStyle: {fontSize: 20},
           chartArea: {'width': '80%'},
@@ -116,7 +120,7 @@ function updateForecast(wetterAktuell) {
           legend: { position: 'none' }
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 		// Plot erstellen
         chart.draw(dataTable, options);
 	});
@@ -129,6 +133,7 @@ function openMenu()
 
 function changeCity(city) {
 	if (city != aktCity) {
+		$("#Seite").html(initLayout);
 		$("#loading").show();
 		aktCity = city;
 		$("#cityTitle").html('Wetter in '+ aktCity);
